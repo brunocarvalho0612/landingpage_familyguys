598f44de40435adbb387379bbb3f48a8bb765514
bruno <brunocarvalhorodrigues@hotmail.com>
2024-10-26 08:39:49 -0300

    novas alterações propostas

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const heroSection = document.querySelector('.hero');
+  const alturaHero = heroSection.clientHeight; 
+
+  window.addEventListener('scroll', function() {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+      ocultaElementosHeader();
+    } else {
+      exibeElementosHeader();
+    }
+  })
+})
+
+function ocultaElementosHeader() {
+  const header = document.querySelector('header');
+  header.classList.add('header--is-hidden');
+}
+  
+function exibeElementosHeader() {
+  const header = document.querySelector('header');
+  header.classList.remove('header--is-hidden');
+}
+
+  
